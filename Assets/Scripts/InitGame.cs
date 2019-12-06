6a86fd1cdabaa8d4c57d568f5cab6d8f2b72b811
Jason Huels <jasonhuels@yahoo.com>
2019-12-06 11:10:12 -0800

    start building prefabs

---
+++ Assets/Scripts/InitGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitGame : MonoBehaviour
+{
+    public GameObject wall;
+    public int columns = 32;
+    public int rows = 32;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+        for(int x=0; x<columns; x++) {
+            for(int y=0; y<rows; y++) {
+                //At each index add a new Vector3 to our list with the x and y coordinates of that position.
+                GameObject instance = Instantiate(wall, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
