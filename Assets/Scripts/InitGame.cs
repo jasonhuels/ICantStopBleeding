10b6847e9e5e9bcc5851bb259cbd47f0bc1b3419
Jason Huels <jasonhuels@yahoo.com>
2019-12-06 12:40:20 -0800

    refactor level generation logic

--- Assets/Scripts/InitGame.cs
+++ Assets/Scripts/InitGame.cs
@@ -6,18 +6,41 @@ public class InitGame : MonoBehaviour
 {
     public GameObject wall;
     public GameObject player;
-    public int columns = 32;
-    public int rows = 32;
+    public int columns = 16;
+    public int rows = 16;
+
+    private char[,] level = {{'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','P','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1'},
+                             {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}};
+                             
 
   // Start is called before the first frame update
   void Start()
     {
         for(int x=0; x<columns; x++) {
             for(int y=0; y<rows; y++) {
-                //At each index add a new Vector3 to our list with the x and y coordinates of that position.
-                if(x==0 || x ==columns-1 || y ==0 || y ==rows-1) {
+                // //At each index add a new Vector3 to our list with the x and y coordinates of that position.
+                // if(x==0 || x ==columns-1 || y ==0 || y ==rows-1) {
+                //     GameObject instance = Instantiate(wall, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                // } else if(x == 6 && y == 6){
+                //     GameObject instance = Instantiate(player, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                // }
+                if(level[x, y] == '1'){
                     GameObject instance = Instantiate(wall, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
-                } else if(x == 6 && y == 6){
+                } else if(level[x,y] =='P'){
                     GameObject instance = Instantiate(player, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
                 }
             }
